bc202d99f8d02b72080990ac9d2bc7891ec5fea3
lucas.cunegundes <lucas.cunegundes@youtan.com.br>
2024-02-02 14:54:01 -0300

    add: back to my condig, add more plugins

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,80 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("trouble").setup({
+			position = "bottom",
+			height = 10,
+			width = 50,
+			icons = true,
+			mode = "workspace_diagnostics",
+			severity = nil,
+			fold_open = "",
+			fold_closed = "",
+			group = true,
+			padding = true,
+			cycle_results = true,
+			action_keys = {
+				-- close = {},
+				close = "q", -- close the list
+				cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+				refresh = "r", -- manually refresh
+				jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
+				open_split = { "<c-x>" }, -- open buffer in new split
+				open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+				open_tab = { "<c-t>" }, -- open buffer in new tab
+				jump_close = { "o" }, -- jump to the diagnostic and close the list
+				toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+				switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+				toggle_preview = "P", -- toggle auto_preview
+				hover = "K", -- opens a small popup with the full multiline message
+				preview = "p", -- preview the diagnostic location
+				open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
+				close_folds = { "zM", "zm" }, -- close all folds
+				open_folds = { "zR", "zr" }, -- open all folds
+				toggle_fold = { "zA", "za" }, -- toggle fold of current file
+				previous = "k", -- previous item
+				next = "j", -- next item
+				help = "?", -- help menu
+			},
+			multiline = true, -- render multi-line messages
+			indent_lines = true, -- add an indent guide below the fold icons
+			win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
+			auto_open = false, -- automatically open the list when you have diagnostics
+			auto_close = false, -- automatically close the list when you have no diagnostics
+			auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+			auto_fold = false, -- automatically fold a file trouble list at creation
+			auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
+			include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions" }, -- for the given modes, include the declaration of the current symbol in the results
+			signs = {
+				-- icons / text used for a diagnostic
+				error = "",
+				warning = "",
+				hint = "",
+				information = "",
+				other = "",
+			},
+			use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+		})
+
+		-- Lua
+		vim.keymap.set("n", "<leader>t", function()
+			require("trouble").toggle()
+		end)
+		vim.keymap.set("n", "<leader>tw", function()
+			require("trouble").toggle("workspace_diagnostics")
+		end)
+		vim.keymap.set("n", "<leader>td", function()
+			require("trouble").toggle("document_diagnostics")
+		end)
+		vim.keymap.set("n", "<leader>tq", function()
+			require("trouble").toggle("quickfix")
+		end)
+		vim.keymap.set("n", "<leader>tl", function()
+			require("trouble").toggle("loclist")
+		end)
+		vim.keymap.set("n", "tr", function()
+			require("trouble").toggle("lsp_references")
+		end)
+	end,
+}
