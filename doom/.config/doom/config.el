90ea9f72e3c43cbe3a7e321d97153b9a709b176d
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-04-15 17:48:05 -0300

    feat(emacs): setting theme to tokyo night

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-tokyo-night)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -50,8 +50,8 @@
 ;;     (setq x y))
 ;;
 (after! org
-  (setq org-directory "~/org/")
-  (setq org-agenda-files "~/org/agenda.org")
+  (setq org-directory "~/Documentos/org/")
+  (setq org-agenda-files "~/Documentos/org/agenda.org")
   (setq org-log-done 'time)
   (require 'org-bullets)
   (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1)))
