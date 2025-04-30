20907213112fa8d76be97702b76e6a537082aef9
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-04-30 17:50:49 -0300

    feat(wezterm): moved config, config background, keybinds

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,85 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- config.color_scheme = "AdventureTime"
+config.font = wezterm.font("JetBrains Mono Nerd Font", { weight = "Bold" })
+config.font_size = 16
+config.default_cursor_style = "BlinkingBlock"
+config.enable_tab_bar = true
+config.pane_focus_follows_mouse = true
+config.detect_password_input = true
+config.window_background_opacity = 0.8
+config.text_background_opacity = 1.0
+config.window_decorations = "TITLE | RESIZE"
+
+config.colors = {
+	foreground = "white",
+}
+
+config.window_frame = {
+	font = wezterm.font("JetBrains Mono Nerd Font", { weight = "Bold" }),
+	font_size = 11,
+	inactive_titlebar_bg = "#000000",
+	active_titlebar_bg = "#000000",
+	inactive_titlebar_fg = "#cccccc",
+	active_titlebar_fg = "#ffffff",
+	inactive_titlebar_border_bottom = "#000000",
+	active_titlebar_border_bottom = "#000000",
+	button_fg = "#cccccc",
+	button_bg = "#000000",
+	button_hover_fg = "#ffffff",
+	button_hover_bg = "#000000",
+}
+
+config.keys = {
+	{
+		key = "v",
+		mods = "ALT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "ALT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "t",
+		mods = "ALT",
+		action = wezterm.action.SpawnTab("DefaultDomain"),
+	},
+	{
+		key = "Y",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	{
+		key = "P",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.ActivateCommandPalette,
+	},
+	{
+		key = "m",
+		mods = "ALT",
+		action = wezterm.action.RotatePanes("Clockwise"),
+	},
+}
+
+-- Switch Tabs
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
+
+-- Move Tabs
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT|SHIFT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+
+return config
