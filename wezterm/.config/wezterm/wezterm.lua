71633964f76da33a51a51385ec0df491c94887d2
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-05-20 15:58:26 -0300

    fix(wezterm): remove max_fps option, bg opacity, and title window

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- config.color_scheme = "AdventureTime"
 config.font = wezterm.font("JetBrains Mono Nerd Font", { weight = "Bold" })
 config.font_size = 12
 config.line_height = 1.2
@@ -10,9 +9,9 @@ config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.pane_focus_follows_mouse = true
 config.detect_password_input = true
-config.window_background_opacity = 0.9
-config.text_background_opacity = 1.0
-config.window_decorations = "RESIZE"
+-- config.window_background_opacity = 0.9
+-- config.text_background_opacity = 1.0
+config.window_decorations = "RESIZE|TITLE"
 config.window_padding = {
 	left = 10,
 	right = 10,
@@ -22,9 +21,6 @@ config.window_padding = {
 
 config.initial_cols = 180
 config.initial_rows = 37
-
-config.max_fps = 120
-
 config.colors = {
 	foreground = "white",
 }
