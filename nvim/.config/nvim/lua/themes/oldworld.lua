6b93d3e19b891a1295b8447c475aeff1a6ff471e
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-10-07 16:30:04 -0300

    Auto-sync: 2025-10-07 16:30:04

--- nvim/.config/nvim/lua/themes/oldworld.lua
+++ nvim/.config/nvim/lua/themes/oldworld.lua
@@ -3,10 +3,18 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
-    variant = 'default', -- default, oled, cooler
+    variant = 'oled', -- default, oled, cooler
     integrations = { -- You can disable/enable integrations
       neo_tree = true,
       neogit = true,
+    },
+    styles = { -- You can pass the style using the format: style = true
+      comments = { italic = true }, -- style for comments
+      keywords = { italic = true }, -- style for keywords
+      identifiers = {}, -- style for identifiers
+      functions = { italic = true, bold = true }, -- style for functions
+      variables = {}, -- style for variables
+      booleans = { bold = true }, -- style for booleans
     },
   },
 }
