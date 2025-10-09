23b62a98a0b17dbd46c62d466c76615526bb66a7
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-10-09 16:15:04 -0300

    Auto-sync: 2025-10-09 16:15:04

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,38 +1,17 @@
-return { -- Collection of various small independent plugins/modules
+return {
   'echasnovski/mini.nvim',
+  version = false,
   config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
-
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
     require('mini.pairs').setup()
-    require('mini.animate').setup()
-    require('mini.clue').setup()
-    require('mini.diff').setup()
+    require('mini.comment').setup()
+    require('mini.indentscope').setup { delay = 50, symbol = '│' }
     require('mini.notify').setup()
-    require('mini.indentscope').setup {
-      delay = 50,
-      symbol = '|',
-    }
-
-    require('mini.comment').setup {
-      mappings = {
-        comment = '<leader>cc',
-        comment_line = '<leader>cc',
-        comment_visual = '<leader>cc',
-        textobject = '<leader>cc',
-      },
-    }
+    require('mini.diff').setup()
+    require('mini.animate').setup()
+    require('mini.fuzzy').setup()
+    require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
 
     require('mini.files').setup {
       mappings = {
@@ -48,10 +27,70 @@ return { -- Collection of various small independent plugins/modules
         trim_left = '<',
         trim_right = '>',
       },
-
-      vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>'),
     }
 
-    require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+    vim.keymap.set('n', '<leader>e', require('mini.files').open, { desc = 'Abrir explorador de arquivos (mini.files)' })
+
+    require('mini.pick').setup {
+      mappings = {
+        move_down = '<C-j>',
+        move_up = '<C-k>',
+        scroll_down = '<C-d>',
+        scroll_up = '<C-u>',
+        confirm = '<CR>',
+        close = '<Esc>',
+      },
+      options = {
+        use_cache = true,
+      },
+    }
+
+    local pickers = {
+      files = function()
+        require('mini.pick').builtin.files()
+      end,
+
+      grep = function()
+        require('mini.pick').builtin.grep_live()
+      end,
+
+      buffers = function()
+        require('mini.pick').builtin.buffers()
+      end,
+
+      help = function()
+        require('mini.pick').builtin.help()
+      end,
+
+      recent = function()
+        require('mini.pick').builtin.oldfiles()
+      end,
+    }
+
+    vim.keymap.set('n', '<leader>ff', pickers.files, { desc = '[F]ind [F]ile' })
+    vim.keymap.set('n', '<leader>fg', pickers.grep, { desc = '[F]ind by [G]rep' })
+    vim.keymap.set('n', '<leader>fb', pickers.buffers, { desc = '[F]ind [B]uffer' })
+    vim.keymap.set('n', '<leader>fh', pickers.help, { desc = '[F]ind [H]elp' })
+    vim.keymap.set('n', '<leader>fr', pickers.recent, { desc = '[F]ind [R]ecent Files' })
+
+    require('mini.clue').setup {
+      triggers = {
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+      },
+      clues = {
+        require('mini.clue').gen_clues.builtin_completion(),
+        require('mini.clue').gen_clues.g(),
+        require('mini.clue').gen_clues.marks(),
+        require('mini.clue').gen_clues.registers(),
+        require('mini.clue').gen_clues.windows(),
+        require('mini.clue').gen_clues.z(),
+        { mode = 'n', keys = '<leader>f', desc = '+find' },
+        { mode = 'n', keys = '<leader>e', desc = '+explorer' },
+      },
+      window = {
+        delay = 100,
+      },
+    }
   end,
 }
