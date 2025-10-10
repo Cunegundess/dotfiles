0ac5efb966d7d7c46a8703c56bdeb3d6832c5dc0
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-10-10 11:00:04 -0300

    Auto-sync: 2025-10-10 11:00:03

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -4,42 +4,40 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
 
-    -- TODO: ve esse MiniDeps
-    MiniDeps.now(function()
-      local pad = function(str, n)
-        return string.rep(' ', n) .. str
-      end
+    local pad = function(str, n)
+      return string.rep(' ', n) .. str
+    end
 
-      -- Function from [echasnovski](https://github.com/echasnovski/nvim).
-      local greeting = function()
-        local hour = tonumber(vim.fn.strftime '%H')
-        -- [04:00, 12:00) - morning, [12:00, 20:00) - day, [20:00, 04:00) - evening
-        local part_id = math.floor((hour + 4) / 8) + 1
-        local day_part = ({ 'evening', 'morning', 'afternoon', 'evening' })[part_id]
-        local username = vim.loop.os_get_passwd()['username'] or 'USERNAME'
-        return ('Good %s, %s'):format(day_part, username)
-      end
+    -- Function from [echasnovski](https://github.com/echasnovski/nvim).
+    local greeting = function()
+      local hour = tonumber(vim.fn.strftime '%H')
+      -- [04:00, 12:00) - morning, [12:00, 20:00) - day, [20:00, 04:00) - evening
+      local part_id = math.floor((hour + 4) / 8) + 1
+      local day_part = ({ 'evening', 'morning', 'afternoon', 'evening' })[part_id]
+      local username = vim.loop.os_get_passwd()['username'] or 'USERNAME'
+      return ('Good %s, %s'):format(day_part, username)
+    end
 
-      local fortune = function()
-        local ok, quote = pcall(function()
-          local f = assert(io.popen('fortune -s', 'r'))
-          local s = assert(f:read '*a')
-          f:close()
-          return s
-        end)
-        return ok and quote or nil
-      end
+    local fortune = function()
+      local ok, quote = pcall(function()
+        local f = assert(io.popen('fortune -s', 'r'))
+        local s = assert(f:read '*a')
+        f:close()
+        return s
+      end)
+      return ok and quote or nil
+    end
 
-      local longest_line = function(s)
-        local lines = vim.fn.split(s, '\n')
-        local lengths = vim.tbl_map(vim.fn.strdisplaywidth, lines)
-        return math.max(unpack(lengths))
-      end
+    local longest_line = function(s)
+      local lines = vim.fn.split(s, '\n')
+      local lengths = vim.tbl_map(vim.fn.strdisplaywidth, lines)
+      return math.max(unpack(lengths))
+    end
 
-      local starter = require 'mini.starter'
-      starter.setup {
-        -- Default values with exception of '-' as I use it to open mini.files.
-        query_updaters = 'abcdefghijklmnopqrstuvwxyz0123456789_.',
+    local starter = require 'mini.starter'
+    starter.setup {
+      -- Default values with exception of '-' as I use it to open mini.files.
+      query_updaters = 'abcdefghijklmnopqrstuvwxyz0123456789_.',
 
     -- stylua: ignore
     items = {
@@ -54,8 +52,8 @@ return {
       },
     },
 
-        header = function()
-          local banner = [[
+      header = function()
+        local banner = [[
 
                  █              █
 
@@ -65,15 +63,15 @@ return {
 █████ ████ █████ ████ █████ █████ ████
 █████ ████ ████████ █████ ████████
 ]]
-          local msg = greeting()
-          local msg_pad = longest_line(banner) - msg:len()
-          return banner .. pad(msg, msg_pad)
-        end,
+        local msg = greeting()
+        local msg_pad = longest_line(banner) - msg:len()
+        return banner .. pad(msg, msg_pad)
+      end,
 
-        -- Fortune slows startup a small amount, but I like it.
-        footer = fortune(),
-      }
-    end)
+      -- Fortune slows startup a small amount, but I like it.
+      footer = fortune(),
+    }
+
     require('mini.surround').setup()
     require('mini.pairs').setup()
     require('mini.comment').setup()
