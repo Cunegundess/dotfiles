09321af3dacae0c6feeb480e989497df7c32afd0
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-11-10 10:15:03 -0300

    Auto-sync: 2025-11-10 10:15:03

--- nvim/.config/nvim/lua/themes/nord.lua
+++ nvim/.config/nvim/lua/themes/nord.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     vim.g.nord_contrast = true
     vim.g.nord_borders = false
-    vim.g.nord_disable_background = false
+    vim.g.nord_disable_background = true
     vim.g.nord_italic = true
     vim.g.nord_uniform_diff_background = true
     vim.g.nord_bold = true
