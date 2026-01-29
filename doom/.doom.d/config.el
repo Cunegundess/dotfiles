7efe8289411326e4073f969553501ca885fab62b
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2026-01-29 09:30:02 -0300

    Auto-sync: 2026-01-29 09:30:02

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -3,7 +3,6 @@
 (setq user-full-name "Lucas Cunegundes"
       user-mail-address "lucascsantana6@gmail.com")
 
-;; (setq doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 12))
 (setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 12))
 
 (custom-set-faces!
@@ -26,108 +25,97 @@
   (add-hook 'org-mode-hook #'org-bullets-mode))
 
 ;; --------------------
-;; LSP / PYTHON / RUFF
+;; PYTHON + EGLOT + BASEDPYRIGHT
 ;; --------------------
-(after! lsp-mode
-  (setq lsp-idle-delay 0.5
-        lsp-log-io nil
-        lsp-enable-file-watchers nil
-        lsp-file-watch-threshold 5000
-        lsp-disabled-clients '(pylsp))
-
-  ;; UI / Code actions
-  (setq lsp-ui-sideline-enable t
-        lsp-ui-doc-enable t))
-
-(after! flycheck
-  (setq flycheck-checker-error-threshold nil))
-
-(after! lsp-pyright
-  (setq lsp-pyright-langserver-command "basedpyright"
-        lsp-pyright-type-checking-mode "standard"
-        lsp-pyright-auto-import-completions t
-        lsp-pyright-auto-search-paths t
-        lsp-pyright-diagnostic-mode "openFilesOnly"
-        ;; lsp-pyright-disable-organize-imports t
-        ;; lsp-pyright-use-library-code-for-types t
-        lsp-pyright-venv-path "."))
-
 (after! python
+  ;; Virtualenv padrão do projeto
   (setq python-shell-virtualenv-root ".venv")
-  (add-hook 'python-mode-hook #'lsp-deferred)
-  (add-hook 'python-mode-hook
-            (lambda ()
-              (when (projectile-project-root)
-                (setq-local lsp-pyright-extra-paths
-                            (vector (projectile-project-root)))))))
+
+  ;; Eglot automático
+  (add-hook 'python-mode-hook #'eglot-ensure)
+  (add-hook 'python-ts-mode-hook #'eglot-ensure)
+
+  ;; Extra paths úteis (monorepo / docker)
+  (add-hook
+   'python-mode-hook
+   (lambda ()
+     (when (projectile-project-root)
+       (setq-local
+        eglot-workspace-configuration
+        `((:python
+           (:analysis
+            :typeCheckingMode "standard"
+            :autoImportCompletions t
+            :autoSearchPaths t
+            :diagnosticMode "openFilesOnly"
+            :extraPaths [,(projectile-project-root)]))
+          (:basedpyright
+           (:analysis
+            :inlayHints
+            (:variableTypes t
+             :functionReturnTypes t
+             :callArgumentNames t
+             :genericTypes t)))))))))
+
+(after! eglot
+  ;; Pyright server
+  (add-to-list 'eglot-server-programs
+               '((python-mode python-ts-mode)
+                 "basedpyright-langserver" "--stdio"))
+
+  ;; Performance
+  (setq eglot-autoshutdown t
+        eglot-events-buffer-size 0))
 
 ;; --------------------
-;; APHELEIA + RUFF
+;; CODE ACTIONS / NAV
+;; --------------------
+(map! :leader
+      "c a" #'eglot-code-actions
+      "c r" #'eglot-rename
+      "c d" #'xref-find-definitions
+      "c R" #'xref-find-references)
+
+;; --------------------
+;; APHELEIA + RUFF (FORMAT)
 ;; --------------------
 (use-package! apheleia
-  :hook (python-mode . apheleia-mode)
+  :hook ((python-mode python-ts-mode) . apheleia-mode)
   :config
   (setf (alist-get 'python-mode apheleia-mode-alist) 'ruff)
   (setf (alist-get 'python-ts-mode apheleia-mode-alist) 'ruff)
+
   (setf (alist-get 'ruff apheleia-formatters)
         '("ruff" "format" "--stdin-filename" filepath))
+
   (setq apheleia-mode nil)
+
   (map! :map python-mode-map
         :localleader
         "f" #'apheleia-format-buffer
         "F" #'apheleia-format-region))
 
+;; --------------------
+;; FLYCHECK + RUFF (LINT)
+;; --------------------
 (after! flycheck
+  (setq flycheck-checker-error-threshold nil)
+
   (flycheck-define-checker python-ruff
     "Ruff linter"
-    :command ("ruff" "check" "--output-format=text" "--stdin-filename" source-original "-")
+    :command ("ruff" "check" "--output-format=text"
+              "--stdin-filename" source-original "-")
     :standard-input t
     :error-patterns
     ((error line-start (file-name) ":" line ":" column ": " (message) line-end))
     :modes (python-mode python-ts-mode))
 
-  (add-hook 'lsp-managed-mode-hook
+  (add-hook 'python-mode-hook
             (lambda ()
-              (when (derived-mode-p 'python-mode 'python-ts-mode)
-                (flycheck-add-next-checker 'lsp 'python-ruff)))))
-
-(map! :leader "c a" #'lsp-execute-code-action)
+              (flycheck-add-next-checker 'python 'python-ruff))))
 
 ;; --------------------
-;; DAP PYTHON
-;; --------------------
-;; (after! dap-mode
-;;   (dap-mode 1)
-;;   (dap-auto-configure-mode 1))
-
-;; (after! dap-ui
-;;   (dap-ui-mode 1))
-
-;; (use-package! dap-python
-;;   :after dap-mode
-;;   :config
-;;   (setq dap-python-debugger 'debugpy)
-
-;;   (defun my/project-root ()
-;;     (or (projectile-project-root) default-directory))
-
-;;   (dap-register-debug-template
-;;    "Python Docker"
-;;    (list
-;;     :type "python"
-;;     :request "attach"
-;;     :name "Python Docker"
-;;     :connect (list :host "127.0.0.1" :port 5678)
-;;     :pathMappings
-;;     (vector
-;;      (list
-;;       :localRoot (my/project-root)
-;;       :remoteRoot "/app"))
-;;     :django t
-    ;; :justMyCode :json-false)))
-
-;; --------------------
-;; DAPE (DEBUG)
+;; DAPE (DEBUG PYTHON / DOCKER)
 ;; --------------------
 (use-package! dape
   :config
@@ -144,15 +132,17 @@
      port 5678
      host "127.0.0.1"
      cwd ,(lambda () (or (projectile-project-root) default-directory))
-     path-mappings (,(lambda ()
-                       (list
-                        (cons
-                         (or (projectile-project-root) default-directory)
-                         "/app"))))))
+     path-mappings
+     (,(lambda ()
+         (list
+          (cons
+           (or (projectile-project-root) default-directory)
+           "/app"))))))
 
+  ;; Config fixa (exemplo projeto específico)
   (add-to-list
    'dape-configs
-   `(python-docker
+   `(python-docker-alianca
      modes (python-mode python-ts-mode)
      command "python"
      command-args ("-m" "debugpy.adapter")
