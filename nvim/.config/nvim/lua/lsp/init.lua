a00e21e5406dd193551d8854eff1b99197a112be
lucas <lucas.cunegundes@proxion.com.br>
2026-04-15 16:00:01 -0300

    Auto-sync: 2026-04-15 16:00:01

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,55 +1,67 @@
 local lspconfig = require("lspconfig")
 
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      runtime = { version = "LuaJIT" },
-      diagnostics = { globals = { "vim" } },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
-    },
-  },
-})
-
 lspconfig.ty.setup({
-  cmd = { "ty", "server" },
+	root_dir = function(fname)
+		return lspconfig.util.root_pattern("pyproject.toml", ".git")(fname)
+	end,
 })
 
 lspconfig.ruff.setup({
-  cmd = { "ruff", "server", "start" },
-  settings = {
-    ruff = {
-      server = {
-        settings = {
-          preview = { pullRequests = true },
-        },
-      },
-    },
-  },
+	root_dir = function(fname)
+		return lspconfig.util.root_pattern("pyproject.toml", "ruff.toml", ".ruff.toml", ".git")(fname)
+	end,
+	init_options = {
+		settings = {
+			fixAll = true,
+		},
+	},
 })
 
-lspconfig.ts_ls.setup({})
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			diagnostics = { globals = { "vim" } },
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp-attach", { clear = false }),
+	callback = function(event)
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+			vim.lsp.inlay_hint.enable(false, { bufnr = event.buf })
+		end
+	end,
+})
+
+vim.lsp.enable({
+	"ty",
+	"ruff",
+	"lua_ls",
+})
 
 vim.diagnostic.config({
-  virtual_lines = false,
-  virtual_text = false,
-  underline = true,
-  update_in_insert = true,
-  severity_sort = true,
-  float = {
-    border = "rounded",
-    source = true,
-  },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "󰅚 ",
-      [vim.diagnostic.severity.WARN] = "󰀪 ",
-      [vim.diagnostic.severity.INFO] = "󰋽 ",
-      [vim.diagnostic.severity.HINT] = "󰌶 ",
-    },
-  },
+	virtual_lines = false,
+	virtual_text = false,
+	underline = true,
+	update_in_insert = true,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+	},
 })
 
 vim.lsp.inlay_hint.enable(false)
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,58 +1,58 @@
 vim.pack.add({
 	-- Themes
-	"catppuccin/nvim",
-	"folke/tokyonight.nvim",
-	"rose-pine/neovim",
-	"rebelot/kanagawa.nvim",
-	"shaunsingh/nord.nvim",
-	"folke/twilight.nvim",
-	"folke/zen-mode.nvim",
-	"y9san9/y9nika.nvim",
-	"datsfilipe/vesper.nvim",
-	"vague-theme/vague.nvim",
-	"projekt0n/github-nvim-theme",
+	"https://github.com/catppuccin/nvim",
+	"https://github.com/folke/tokyonight.nvim",
+	"https://github.com/rose-pine/neovim",
+	"https://github.com/rebelot/kanagawa.nvim",
+	"https://github.com/shaunsingh/nord.nvim",
+	"https://github.com/folke/twilight.nvim",
+	"https://github.com/folke/zen-mode.nvim",
+	"https://github.com/y9san9/y9nika.nvim",
+	"https://github.com/datsfilipe/vesper.nvim",
+	"https://github.com/vague-theme/vague.nvim",
+	"https://github.com/projekt0n/github-nvim-theme",
 
 	-- LSP & Completion
-	"saghen/blink.cmp",
-	"L3MON4D3/LuaSnip",
-	"neovim/nvim-lspconfig",
+	"https://github.com/saghen/blink.cmp",
+	"https://github.com/L3MON4D3/LuaSnip",
+	"https://github.com/neovim/nvim-lspconfig",
 
 	-- UI
-	"echasnovski/mini.nvim",
-	"echasnovski/mini.icons",
-	"folke/which-key.nvim",
+	"https://github.com/echasnovski/mini.nvim",
+	"https://github.com/echasnovski/mini.icons",
+	"https://github.com/folke/which-key.nvim",
 
 	-- File navigation
-	"stevearc/oil.nvim",
-	"nvim-lua/plenary.nvim",
-	"ibhagwan/fzf-lua",
+	"https://github.com/stevearc/oil.nvim",
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/ibhagwan/fzf-lua",
 
 	-- Git
-	"lewis6991/gitsigns.nvim",
-	"NeogitOrg/neogit",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/NeogitOrg/neogit",
 
 	-- Formatting
-	"stevearc/conform.nvim",
-	"williamboman/mason.nvim",
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
 
 	-- Syntax
-	"nvim-treesitter/nvim-treesitter",
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	"nvim-treesitter/nvim-treesitter-context",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+	"https://github.com/nvim-treesitter/nvim-treesitter-context",
 
 	-- Debug
-	"mfussenegger/nvim-dap",
-	"rcarriga/nvim-dap-ui",
-	"nvim-neotest/nvim-nio",
-	"mfussenegger/nvim-dap-python",
+	"https://github.com/mfussenegger/nvim-dap",
+	"https://github.com/rcarriga/nvim-dap-ui",
+	"https://github.com/nvim-neotest/nvim-nio",
+	"https://github.com/mfussenegger/nvim-dap-python",
 
 	-- IA
-	"github/copilot.vim",
+	"https://github.com/github/copilot.vim",
 
 	-- Utils
-	"esmuellert/codediff.nvim",
-	"m00qek/baleia.nvim",
+	"https://github.com/esmuellert/codediff.nvim",
+	"https://github.com/m00qek/baleia.nvim",
 })
 
 vim.defer_fn(function()
@@ -64,7 +64,6 @@ vim.defer_fn(function()
 	pcall(require, "plugins.gitsigns")
 	pcall(require, "plugins.fzf")
 	pcall(require, "plugins.oil")
-	pcall(require, "plugins.blink")
 
 	local ok_dap, dap_config = pcall(require, "plugins.dap")
 	if ok_dap and type(dap_config) == "function" then
