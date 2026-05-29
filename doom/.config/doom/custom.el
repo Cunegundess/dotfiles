db67e6e15c58a5571a13f4c2704b856a97930872
lucas <lucas.cunegundes@proxion.com.br>
2026-05-29 14:30:03 -0300

    Auto-sync: 2026-05-29 14:30:03

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(badger-theme bubbleberry-theme kind-icon lsp-ui multiple-cursors)))
+   '(badger-theme bubbleberry-theme eca eshell-vterm kind-icon lsp-ui
+     multiple-cursors)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
