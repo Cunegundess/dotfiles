a86fcae8bc9ba9a0255e51bc6cc7054c55c914c1
lucas <lucas.cunegundes@proxion.com.br>
2026-06-03 11:00:02 -0300

    Auto-sync: 2026-06-03 11:00:02

--- nvim/.config/nvim/lua/lsp/kotlin.lua
+++ nvim/.config/nvim/lua/lsp/kotlin.lua
@@ -7,9 +7,19 @@ local root_markers = {
 	"pom.xml",
 }
 
+local home = vim.fn.expand("$HOME")
+
 return {
-	cmd = { "intellij-server", "--stdio" },
+	cmd = {
+		"intellij-server",
+		"--stdio",
+		"--system-path",
+		home .. "/.cache/kotlin-lsp/",
+	},
 	filetypes = { "kotlin" },
 	root_markers = root_markers,
 	single_file_support = true,
+	cmd_env = {
+		IJ_JAVA_OPTIONS = "-Xmx4g -Xms2g -XX:+UseG1GC -Dcom.jetbrains.ls.imports.gradle.offline=true",
+	},
 }
