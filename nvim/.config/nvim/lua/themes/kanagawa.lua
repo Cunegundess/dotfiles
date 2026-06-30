fd79b6002e84a683bc87ee0e0be8571bf3962dba
lucas <lucas.cunegundes@proxion.com.br>
2026-06-30 17:00:02 -0300

    Auto-sync: 2026-06-30 17:00:02

---
+++ nvim/.config/nvim/lua/themes/kanagawa.lua
@@ -0,0 +1,12 @@
+require("kanagawa").setup({
+	compile = false,
+	undercurl = true,
+	commentStyle = { italic = true },
+	functionStyle = { italic = true },
+	keywordStyle = { italic = true },
+	statementStyle = { bold = true },
+	typeStyle = { bold = true },
+	transparent = false,
+	dimInactive = true,
+	terminalColors = true,
+})
---
+++ nvim/.config/nvim/lua/themes/vesper.lua
@@ -0,0 +1,10 @@
+require("vesper").setup({
+	transparent = false,
+	italics = {
+		comments = true,
+		keywords = true,
+		functions = false,
+		strings = true,
+		variables = false,
+	},
+})
