3ffdd304e749a53a5642004a6da38a8114e66f3a
lucas <lucas.cunegundes@proxion.com.br>
2026-07-01 10:45:02 -0300

    Auto-sync: 2026-07-01 10:45:02

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -4,8 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ai-code autothemer badger-theme bubbleberry-theme eca eshell-vterm kind-icon
-     lsp-ui multiple-cursors)))
+   '(ai-code autothemer badger-theme bubbleberry-theme eca ef-themes eshell-vterm
+     kind-icon lsp-ui multiple-cursors nordic-night-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
