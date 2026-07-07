ec985438a2569ecba155295472db1221235fd304
lucas <lucas.cunegundes@proxion.com.br>
2026-07-07 11:45:01 -0300

    Auto-sync: 2026-07-07 11:45:01

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,32 +1,3 @@
-_G.Config = {}
-
-local gr = vim.api.nvim_create_augroup('custom-config', {})
-Config.new_autocmd = function(event, pattern, callback, desc)
-  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
-  vim.api.nvim_create_autocmd(event, opts)
-end
-
-Config.on_packchanged = function(plugin_name, kinds, callback, desc)
-  local f = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then return end
-    if not ev.data.active then vim.cmd.packadd(plugin_name) end
-    callback(ev.data)
-  end
-  Config.new_autocmd('PackChanged', '*', f, desc)
-end
-
-local safe = function(fn, ...)
-  local ok, result = pcall(fn, ...)
-  if not ok then
-    vim.notify('[init] ' .. tostring(result), vim.log.levels.WARN)
-  end
-  return result
-end
-
-Config.now = function(f) safe(f) end
-Config.later = function(f) vim.defer_fn(function() safe(f) end, 0) end
-
 vim.pack.add({
   -- Core
   'https://github.com/echasnovski/mini.nvim',
@@ -37,12 +8,6 @@ vim.pack.add({
   -- LSP & Completion
   'https://github.com/saghen/blink.cmp',
   'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/williamboman/mason.nvim',
-  'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
-
-  -- Treesitter
-  'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
 
   -- DAP
   'https://github.com/mfussenegger/nvim-dap',
@@ -59,16 +24,55 @@ vim.pack.add({
   'https://github.com/ibhagwan/fzf-lua',
   'https://github.com/nvim-lua/plenary.nvim',
 
-  -- Formatting
-  'https://github.com/stevearc/conform.nvim',
-
   -- Git
+  'https://github.com/tpope/vim-fugitive',
+  'https://github.com/tpope/vim-rhubarb',
   'https://github.com/lewis6991/gitsigns.nvim',
-  'https://github.com/NeogitOrg/neogit',
+  'https://github.com/justinmk/guh.nvim',
 
-  -- UI
-  'https://github.com/folke/which-key.nvim',
+  -- tpope essentials
+  'https://github.com/tpope/vim-repeat',
+  'https://github.com/tpope/vim-eunuch',
+  'https://github.com/tpope/vim-rsi',
+  'https://github.com/tpope/vim-unimpaired',
+  'https://github.com/tpope/vim-dispatch',
+
+  -- Justin's utils
+  'https://github.com/justinmk/vim-sneak.git',
+  'https://github.com/justinmk/vim-ipmotion.git',
+  'https://github.com/justinmk/vim-gtfo.git',
+  'https://github.com/justinmk/nvim-repl',
+  'https://github.com/justinmk/vim-ug',
+
+  -- Text objects & editing
+  'https://github.com/tommcdo/vim-lion',
+  'https://github.com/tommcdo/vim-exchange',
+  'https://github.com/haya14busa/vim-edgemotion',
+  'https://github.com/inkarkat/vim-ingo-library',
+  'https://github.com/inkarkat/vim-mark',
+  'https://github.com/mfussenegger/nvim-treehopper',
+  'https://github.com/AndrewRadev/linediff.vim',
+
+  -- Navigation
   'https://github.com/stevearc/oil.nvim',
-  'https://github.com/folke/trouble.nvim',
+  'https://github.com/stevearc/quicker.nvim',
+  'https://github.com/mfussenegger/nvim-qwahl',
+
+  -- Diff
+  'https://github.com/barrettruth/diffs.nvim',
+
+  -- Misc
+  'https://github.com/folke/zen-mode.nvim',
+  'https://github.com/tpope/vim-obsession',
+  'https://github.com/tpope/vim-projectionist',
+  'https://github.com/tpope/vim-characterize',
+  'https://github.com/tpope/vim-apathy',
+})
+
+vim.pack.add({
+  'https://github.com/chrisbra/Colorizer',
+  'https://github.com/mbbill/undotree',
   'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+}, {
+  load = function() end,
 })
