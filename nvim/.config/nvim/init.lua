f0f45ca144b7d2f6bec9b154456c8daca666b300
lucas <lucas.cunegundes@proxion.com.br>
2026-07-28 12:00:02 -0300

    Auto-sync: 2026-07-28 12:00:02

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -30,6 +30,17 @@ vim.pack.add({
 
   -- Diff
   'https://github.com/barrettruth/diffs.nvim',
+
+  {
+    src = 'https://github.com/JavaHello/spring-boot.nvim',
+    version = '218c0c26c14d99feca778e4d13f5ec3e8b1b60f0',
+  },
+
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/nvim-java/nvim-java',
 })
+
+require('java').setup()
+vim.lsp.enable('jdtls')
 
 require('theme')
